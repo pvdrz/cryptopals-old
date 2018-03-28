fd73a98141256d10d12fd350e8f927965fb6b39a
Christian Poveda <christianpoveda@protonmail.com>
2018-03-28 15:52:34 -0500

    solved challenge 7

---
+++ tests/sets/set_01/ch_07.rs
@@ -0,0 +1,15 @@
+use cryptopals::cyphers::rijndael::Cypher;
+use cryptopals::cyphers::rijndael::Mode::ECB;
+use cryptopals::encoding::base64::Base64;
+use cryptopals::utils::io::load_as_bytes;
+
+#[test]
+fn challenge_07() {
+    let b64 = Base64::new();
+    let key = b"YELLOW SUBMARINE".to_vec();
+    let cyphertext = b64.decode(&load_as_bytes("data/7.txt").unwrap());
+    let cypher = Cypher::new(key, ECB);
+    let plaintext = cypher.decrypt(&cyphertext);
+    let result: &[u8] = b"I\'m back and I\'m ringin\' the bell";
+    assert_eq!(result, &plaintext[0..result.len()]);
+}
--- tests/sets/set_01/mod.rs
+++ tests/sets/set_01/mod.rs
@@ -4,3 +4,4 @@ pub mod ch_03;
 pub mod ch_04;
 pub mod ch_05;
 pub mod ch_06;
+pub mod ch_07;
