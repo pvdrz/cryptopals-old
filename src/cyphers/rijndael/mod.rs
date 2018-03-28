04e477c10bc26ca782b484d68db3f11f4aa67318
Christian Poveda <christianpoveda@protonmail.com>
2018-03-28 15:40:18 -0500

    added modes of operation for aes

--- src/cyphers/rijndael/mod.rs
+++ src/cyphers/rijndael/mod.rs
@@ -2,22 +2,30 @@ mod encrypt;
 mod decrypt;
 
 use cyphers::xor::fixed_xor;
+use rayon::slice::ParallelSlice;
+use rayon::iter::ParallelIterator;
+
+pub enum Mode {
+    ECB
+}
 
 pub struct Cypher {
     key: Vec<u8>,
-    schedule: Vec<Vec<u8>>
+    schedule: Vec<Vec<u8>>,
+    mode: Mode
 }
 
 impl Cypher {
-    pub fn new(key: Vec<u8>) -> Self {
+    pub fn new(key: Vec<u8>, mode: Mode) -> Self {
         let schedule = encrypt::key_schedule(&key);
         Cypher {
             key: key,
-            schedule: schedule
+            schedule: schedule,
+            mode: mode
         }
     }
 
-    pub fn encrypt(&self, plaintext: &[u8]) -> Vec<u8> {
+    fn encrypt_block(&self, plaintext: &[u8]) -> Vec<u8> {
         let mut out = fixed_xor(plaintext, &self.key);
         for (i, key) in self.schedule.iter().enumerate() {
             out = encrypt::byte_substitution(&out);
@@ -30,7 +38,7 @@ impl Cypher {
         out
     }
 
-    pub fn decrypt(&self, cyphertext: &[u8]) -> Vec<u8> {
+    fn decrypt_block(&self, cyphertext: &[u8]) -> Vec<u8> {
         let mut out = cyphertext.to_vec();
         for (i, key) in self.schedule.iter().enumerate().rev() {
             out = fixed_xor(&out, &key);
@@ -43,5 +51,18 @@ impl Cypher {
         out = fixed_xor(&out, &self.key);
         out
     }
-}
 
+    pub fn encrypt(&self, plaintext: &[u8]) -> Vec<u8> {
+        match self.mode {
+            Mode::ECB => plaintext.par_chunks(16)
+                .flat_map(|b| self.encrypt_block(b)).collect()
+        }
+    }
+
+    pub fn decrypt(&self, cyphertext: &[u8]) -> Vec<u8> {
+        match self.mode {
+            Mode::ECB => cyphertext.par_chunks(16)
+                .flat_map(|b| self.decrypt_block(b)).collect()
+        }
+    }
+}
--- tests/unit/cyphers/rijndael.rs
+++ tests/unit/cyphers/rijndael.rs
@@ -1,4 +1,5 @@
 use cryptopals::cyphers::rijndael::Cypher;
+use cryptopals::cyphers::rijndael::Mode::ECB;
 use cryptopals::encoding::base16::Base16;
 
 #[test]
@@ -9,7 +10,7 @@ fn rijndael_0() {
     let plaintext =  b16.decode(b"f34481ec3cc627bacd5dc3fb08f273e6");
     let cyphertext = b16.decode(b"0336763e966d92595a567cc9ce537f5e");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -23,7 +24,7 @@ fn rijndael_1() {
     let plaintext =  b16.decode(b"9798c4640bad75c7c3227db910174e72");
     let cyphertext = b16.decode(b"a9a1631bf4996954ebc093957b234589");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -37,7 +38,7 @@ fn rijndael_2() {
     let plaintext =  b16.decode(b"96ab5c2ff612d9dfaae8c31f30c42168");
     let cyphertext = b16.decode(b"ff4f8391a6a40ca5b25d23bedd44a597");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -51,7 +52,7 @@ fn rijndael_3() {
     let plaintext =  b16.decode(b"6a118a874519e64e9963798a503f1d35");
     let cyphertext = b16.decode(b"dc43be40be0e53712f7e2bf5ca707209");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -65,7 +66,7 @@ fn rijndael_4() {
     let plaintext =  b16.decode(b"cb9fceec81286ca3e989bd979b0cb284");
     let cyphertext = b16.decode(b"92beedab1895a94faa69b632e5cc47ce");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -79,7 +80,7 @@ fn rijndael_5() {
     let plaintext =  b16.decode(b"b26aeb1874e47ca8358ff22378f09144");
     let cyphertext = b16.decode(b"459264f4798f6a78bacb89c15ed3d601");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
@@ -93,7 +94,7 @@ fn rijndael_6() {
     let plaintext =  b16.decode(b"58c8e00b2631686d54eab84b91f0aca1");
     let cyphertext = b16.decode(b"08a4e2efec8a8e3312ca7460b9040bbf");
 
-    let cypher = Cypher::new(key);
+    let cypher = Cypher::new(key, ECB);
 
     assert_eq!(cyphertext, cypher.encrypt(&plaintext));
     assert_eq!(plaintext, cypher.decrypt(&cyphertext));
