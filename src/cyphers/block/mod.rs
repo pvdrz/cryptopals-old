37f422f862e8f56be0f5f0abb4cd6b3f789f2b71
Christian Poveda <christianpoveda@protonmail.com>
2018-03-30 19:00:12 -0500

    added padding to block cypher

--- src/cyphers/block/mod.rs
+++ src/cyphers/block/mod.rs
@@ -1,6 +1,7 @@
 use cyphers::xor::fixed_xor;
 use rayon::slice::ParallelSlice;
 use rayon::iter::ParallelIterator;
+use utils::padding::pkcs7;
 
 pub mod aes;
 
@@ -11,6 +12,7 @@ pub enum OperationMode {
 
 pub struct BlockCypher<T: Cypher> {
     operation_mode: OperationMode,
+    blocksize: usize,
     cypher: T
 }
 
@@ -18,18 +20,20 @@ impl<T: Cypher> BlockCypher<T> {
     pub fn new(cypher: T, operation_mode: OperationMode) -> Self {
         BlockCypher {
             operation_mode: operation_mode,
+            blocksize: cypher.blocksize(),
             cypher: cypher
         }
     }
     pub fn encrypt(&self, plaintext: &[u8]) -> Vec<u8> {
+        let plaintext: &[u8] = &pkcs7(plaintext, self.blocksize);
         match self.operation_mode {
             OperationMode::ECB => plaintext
-                .par_chunks(self.cypher.blocksize())
+                .par_chunks(self.blocksize)
                 .flat_map(|b| self.cypher.encrypt_block(b)).collect(),
             OperationMode::CBC(ref init_vec) => {
                 let mut vec = init_vec.clone();
                 let mut cyphertext = Vec::new();
-                for block in plaintext.chunks(self.cypher.blocksize()) {
+                for block in plaintext.chunks(self.blocksize) {
                     vec = self.cypher.encrypt_block(&fixed_xor(block, &vec));
                     cyphertext.extend_from_slice(&vec);
                 }
@@ -40,12 +44,12 @@ impl<T: Cypher> BlockCypher<T> {
     pub fn decrypt(&self, cyphertext: &[u8]) -> Vec<u8> {
         match self.operation_mode {
             OperationMode::ECB => cyphertext
-                .par_chunks(self.cypher.blocksize())
+                .par_chunks(self.blocksize)
                 .flat_map(|b| self.cypher.decrypt_block(b)).collect(),
             OperationMode::CBC(ref init_vec) => {
                 let mut vec = init_vec.clone();
                 let mut plaintext = Vec::new();
-                for block in cyphertext.chunks(self.cypher.blocksize()) {
+                for block in cyphertext.chunks(self.blocksize) {
                     plaintext.extend_from_slice(
                         &fixed_xor(&self.cypher.decrypt_block(&block), &vec));
                     vec = block.to_vec();
--- src/utils/padding.rs
+++ src/utils/padding.rs
@@ -1,6 +1,9 @@
 pub fn pkcs7(buf: &[u8], blocksize: usize) -> Vec<u8> {
     let mut vec = buf.to_vec();
-    let n = blocksize - (buf.len() % blocksize);
+    let n = match buf.len() % blocksize {
+        0 => 0,
+        k => blocksize - k
+    };
     vec.extend_from_slice(&vec![n as u8; n]);
     vec
 }
